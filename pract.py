f8b74e01b76d0d618180d0f1f7cf0dc05ad2ec09
Студент212-1 <212-1@mveu.local>
2024-09-26 18:47:10 +0400

    practice oop

---
+++ pract.py
@@ -0,0 +1,41 @@
+
+# //pract1
+
+# /zadanie 1
+
+# class Goods:
+#     def set_types(self, title, weight, tp, price):
+#         self.title = title
+#         self.weight = weight
+#         self.tp = tp
+#         self.price = price
+        
+#     def display(self):
+#         print(f"{self.tp} {self.title} весит {self.weight} грамм, стоит {self.price}")
+        
+# eat = Goods()
+
+# eat.set_types("Мороженое", 151, "Еда", 12312)
+# eat.display()
+    
+    
+    
+    
+# /zadanie 2
+
+# class Car():
+#     pass
+
+# object = Car()
+
+# object.model = 'ВАЗ 2114'
+# object.color = 'Средний серо-зеленый металлик'
+# object.number = 'Х764СТ18'
+
+# print(f"Машина {object.model}, цвета {object.color}, гос.номер {object.number}")
+        
+
+
+
+
+
