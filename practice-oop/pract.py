cf42bcd72f43551edccbc8811374f069d78326dc
sayykoo <137409213+sayykoo@users.noreply.github.com>
2024-10-07 18:14:16 +0400

    Add files via upload

---
+++ practice-oop/pract.py
@@ -0,0 +1,38 @@
+
+# # //pract1
+
+# /zadanie 1
+
+class Goods:
+    title = "Мороженое"
+    weight = 151 
+    tp = "Еда"
+    price = 12321
+        
+eat = Goods()
+
+eat.price = 112
+eat.weight = 12
+print(f"{eat.tp} {eat.title} - вес {eat.weight} грамм, цена {eat.price} тенге")
+    
+    
+    
+    
+# # /zadanie 2
+
+# class Car():
+#     pass
+
+# object = Car()
+
+# object.model = 'ВАЗ 2114'
+# object.color = 'Средний серо-зеленый металлик'
+# object.number = 'Х764СТ18'
+
+# print(f"Машина {object.model}, цвета {object.color}, гос.номер {object.number}")
+        
+
+
+
+
+
